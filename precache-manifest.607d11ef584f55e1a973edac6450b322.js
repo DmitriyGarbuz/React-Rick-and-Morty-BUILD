a3a0fa79bd97e8fc9bef416a6ec1211712b9f5aa
Dmitriy Garbuz <51447305+DmitriyGarbuz@users.noreply.github.com>
2019-09-06 00:24:32 +0300

    Build React-create-app

---
+++ precache-manifest.607d11ef584f55e1a973edac6450b322.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8c56e6007b71c67479618875792f355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ea1efd82bd36a8bae2d2",
+    "url": "/static/js/2.f071d29b.chunk.js"
+  },
+  {
+    "revision": "023429ccddd598a98ad1",
+    "url": "/static/js/main.278b42d2.chunk.js"
+  },
+  {
+    "revision": "e9657d0c8f64c11c2833",
+    "url": "/static/js/runtime~main.5432e2b6.js"
+  },
+  {
+    "revision": "bd903cb1812f67e62caa922678541ba9",
+    "url": "/static/media/RickAudio.bd903cb1.mp3"
+  }
+]);
